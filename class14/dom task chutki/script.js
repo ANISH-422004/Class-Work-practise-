11faf1adc2783aa755c22bdd0b4ab8dabbdd67b0
ANISH-422004 <anishbhattacharyagtl@gmail.com>
2024-11-26 23:05:03 +0530

    scss me thode changes kare

--- class14/dom task chutki/script.js
+++ class14/dom task chutki/script.js
@@ -9,8 +9,9 @@ var resetactivate = false;
 
 body.addEventListener("keypress",(dets)=>{
     console.log(dets.key);
-    if(dets.key == "d"){
-        movex += 10;
+    if (0<=movex && movex<=100 && 0<=movey && movey<=100){
+        if(dets.key == "d"){
+            movex += 10;
     }
     if(dets.key == "a"){
         movex -= 10;
@@ -21,21 +22,27 @@ body.addEventListener("keypress",(dets)=>{
     if(dets.key == "w"){
         movey -= 10;
     }
+    }
+
     console.log(movex); 
     console.log(movey);
+
     img.style.left = `${movex}%`;
     img.style.top = `${movey}%`;
 
 
-     if ( movex >= 90 && movey >= 90){
+     if ( movex >= 80 && movey >= 80){
         img.style.opacity = 0;
+        img.style.display = "none";
         button.style.display = "block";
         resetactivate = true;
+
      }
 
      if (resetactivate){
         button.addEventListener("click",()=>{
             img.style.opacity = 1;
+            img.style.display = "block"
             button.style.display = "none";
             resetactivate = false;
             movex = 0;
