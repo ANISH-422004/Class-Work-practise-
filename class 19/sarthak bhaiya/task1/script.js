17ada9d7cab9b9640231fb0101d81c69099a5e4e
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-02 14:38:24 +0530

    class 19 task1 done but its buggy

---
+++ class 19/sarthak bhaiya/task1/script.js
@@ -0,0 +1,26 @@
+
+var parent = document.querySelectorAll(".parent");
+
+console.log(parent[0].childNodes);
+
+parent.forEach(function(ele){
+    // console.log();   
+    ele.addEventListener("mouseover",function(){
+        ele.style.backgroundColor = "black";
+        ele.childNodes[1].style.color = "white";
+    })
+    ele.addEventListener("mouseout",function(){
+        ele.style.backgroundColor = "white";
+        ele.childNodes[1].style.color = "black";
+        ele.childNodes[5].style.opacity = 0;
+    })
+    ele.addEventListener("mousemove",function(dets){
+        // console.log(dets.x)
+        // console.log(dets.y);
+        // console.log();      
+        ele.childNodes[5].style.opacity = 1;
+        ele.childNodes[5].style.left = `${dets.x}px`;
+        // ele.childNodes[5].style.top = `${dets.y}px`;
+
+    })
+})
