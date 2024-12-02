436e6bbb88debdead09a17cf2951aab2a98d61be
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-02 17:34:30 +0530

    up to task 2 done

--- class 19/sarthak bhaiya/task1/script.js
+++ class 19/sarthak bhaiya/task1/script.js
@@ -5,15 +5,16 @@ console.log(parent[0].childNodes);
 
 parent.forEach(function(ele){
     // console.log();   
-    ele.addEventListener("mouseover",function(){
+    ele.addEventListener("mouseenter",function(){
         ele.style.backgroundColor = "black";
         ele.childNodes[1].style.color = "white";
     })
-    ele.addEventListener("mouseout",function(){
+    ele.addEventListener("mouseleave",function(){
         ele.style.backgroundColor = "white";
         ele.childNodes[1].style.color = "black";
         ele.childNodes[5].style.opacity = 0;
     })
+
     ele.addEventListener("mousemove",function(dets){
         // console.log(dets.x)
         // console.log(dets.y);
