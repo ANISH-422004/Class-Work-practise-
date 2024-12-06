303beb1d1364bbb6c16460406ba0d833b7d35945
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-07 03:49:52 +0530

    section10 done

---
+++ class 23/sarthabhaiya/class1/src/components/Section10.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Section10 = () => {
+  return (
+    <>
+    
+        <div className='bg-[#F4F4F1] min-h-[60vh] px-10 py-10'>
+                <div id="top" className='py-2  '>
+                    <h3 className='text-2xl font-semibold'>Did some cool stuff for</h3>
+                    <hr className="h-[2rem]" />
+
+                </div>
+                <div id="bottom" className='h-[80%] flex justify-between relative'>
+                    <div className="coolstuff w-[30%] text-[#A3A4A7]">
+                        <h3 className='my-3 text-xl font-medium '>Bewakingsdienst van MOOK</h3>
+                        <h3 className='my-3 text-xl font-medium '>Van der Heijden bouw</h3>
+                        <h3 className='my-3 text-xl font-medium '>Speyz</h3>
+                        <h3 className='my-3 text-xl font-medium '>Zeth</h3>
+                    </div>
+                    <div className="coolstuff w-[30%] text-[#A3A4A7]">
+                        <h3 className='my-3 text-xl font-medium '>Ozo Verbindzorg</h3>
+                        <h3 className='my-3 text-xl font-medium '>Signal Stream</h3>
+                        <h3 className='my-3 text-xl font-medium '>
+                        Gaaf creaties</h3>
+                        <h3 className='my-3 text-xl font-medium '>
+                        Lunenburg Events</h3>
+                    </div>
+                    <div className="coolstuff w-[30%] text-[#A3A4A7]">
+                        <h3 className='my-3 text-xl font-medium '>Twofiftyk</h3>
+                        <h3 className='my-3 text-xl font-medium '>
+                        Adekwaad</h3>
+                        <h3 className='my-3 text-xl font-medium '>Zoet moment</h3>
+                        <h3 className='my-3 text-xl font-medium '>
+                        Forex library</h3>
+                    </div>
+                    
+                    
+                        
+                    <img  className='absolute top-[38%] left-[85%] mei ' src="https://cdn.prod.website-files.com/6454cbf2dcc426c6ec47ef06/64bbbf416decd23360ebb88c_get-in-touch-badge.svg" alt="" />
+                           
+                  
+                </div>
+        </div>
+
+    </>
+  )
+}
+
+export default Section10
