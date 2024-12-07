1450bad465798add0866a4357548ee6ac864aced
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-07 13:45:38 +0530

    done

--- class 23/sarthabhaiya/class1/src/App.jsx
+++ class 23/sarthabhaiya/class1/src/App.jsx
@@ -11,6 +11,7 @@ import Section8 from './components/Section8'
 import Section9 from './components/Section9'
 import Section10 from './components/Section10'
 import Section11 from './components/Section11'
+
 const App = () => {
   return (
     <div>
