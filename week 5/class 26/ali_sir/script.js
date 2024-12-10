7eef6542ee1b7182f1ba48b2d9603b0ccf01d1e0
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-10 13:24:10 +0530

    ali bhaiya class done

--- week 5/class 26/ali_sir/script.js
+++ week 5/class 26/ali_sir/script.js
@@ -1,1 +1,38 @@
-console.log("anish")
+
+//////Q37.isbn number 
+// its a 10 digit number and 
+// conditions to verify it ISBN number
+// 1st its should have 10 digi  
+
+
+// Array : it is a data structure 
+
+
+//37. sum of elements of array 
+
+
+// let a = []
+// let size = Number(prompt("size of array"))
+// let sum = 0 
+// for (let i = 0 ; i<size ; i++){
+//     a[i] = Number(prompt(`Enter element ${i+1}`))
+//     sum += a[i]
+// }
+
+// console.log(a , sum );
+
+
+
+// 38. greatest value of array
+
+// let a = [1,23,4 ,4,5,6,7,123,456]
+
+// let max = a[0]
+
+// for (let i = 1 ; i<a.length ; i++){
+//     if(a[i] > max){
+//         max = a[i]
+//     }
+// }
+
+// console.log(max);
