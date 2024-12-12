c2fcffdd21abe9d6c44891f1179196d98ee34c92
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-12 13:39:42 +0530

    ali sir class done

---
+++ week 5/calss 28/ali sir/script.js
@@ -0,0 +1,31 @@
+// sagriagte +vs and -ve
+// let arr = [1,-3,4,8,-9,10,-10,-10,10,20]
+// let j=0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]<0){
+//         let temp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = temp;
+//         j++;
+//     }
+// }
+// console.log(arr)
+
+
+// sagrigate 0s asnd 1s in an  Array wheres elemts are only 1s and 0s
+// let arr = [1,1,0,1,0,1,1,0,0,0,1]
+// let j=0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]===0){
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++;
+//     }
+// }
+// console.log(arr)
+
+
+/// find maximum number of 1s seen in array
+
+
