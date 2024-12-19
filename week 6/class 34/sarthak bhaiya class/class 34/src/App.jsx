4c03b52d75a365416e0b18a19840aced103c8fb3
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-19 16:48:29 +0530

    unfinished

--- week 6/class 34/sarthak bhaiya class/class 34/src/App.jsx
+++ week 6/class 34/sarthak bhaiya class/class 34/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Header from './components/Header/Header'
+import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
-    <div>App</div>
+    <> 
+      <Header></Header>
+      <Routes>
+        <Route to="/" v></Route>
+      </Routes>
+     </>
   )
 }
 
