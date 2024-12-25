5368a143dedec3b3ee0501581e8898c7ccce181f
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-25 16:10:14 +0530

    Aliclass Over

---
+++ week 7/class 39/Alibhaiya/class39.js
@@ -0,0 +1,56 @@
+//60 Basic Qestions
+
+
+//52.ReverseString 
+//Print the number of vowel consonants number and space
+
+
+// let str = "hello world 2345 anish"
+// let vol = 0
+// let space = 0
+// let consto = 0
+// let num = 0
+// for(let i = 0; i < str.length; i++){
+//     if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i'|| str[i] =='o'|| str[i] == 'u'){
+//         vol += 1
+//     }
+//     else if(str[i] == ' '){
+//         space += 1
+//     }
+//     else if(str.charAt(i) >= 0 && str.charAt(i) <= 9)
+//     {
+//         num += 1
+//     }
+//     else{
+//         consto += 1
+//     }
+// }
+
+// console.log("Vowels: " + vol)
+
+// console.log("Spaces: " + space)
+
+// console.log("Consonants: " + consto)
+
+// console.log("numbers: " + num);
+
+
+
+
+//Q53.GIVEN String is Paliaindromic or NONPaliaindromic 
+
+// let s = "anish"
+// let f= 0
+// let l= s.length-1
+// let p = "p"
+// while(f<=l){
+//     if(s[f]!=s[l]){
+//         p = "Np";
+//         break
+        
+//     }
+//     f++
+//     l--
+// }
+// console.log(p);
+
