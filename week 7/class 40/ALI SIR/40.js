c93ee3aa1943016c0e517451606166589d306b6b
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-26 17:45:28 +0530

    class Done

---
+++ week 7/class 40/ALI SIR/40.js
@@ -0,0 +1,16 @@
+// Q55. flip word 
+// str = "AnIsH"
+// ans=""
+// for (let i=0 ; i<str.length ; i++){
+//     if(str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90 ){
+//      ans = ans + String.fromCharCode(str.charCodeAt(i)+32)   
+        
+//     }
+//     else {
+//      ans = ans + String.fromCharCode(str.charCodeAt(i)-32)   
+
+//     }
+// }
+// console.log(ans);
+
+//Q.56 
