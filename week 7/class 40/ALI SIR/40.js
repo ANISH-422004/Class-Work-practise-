cc5c56297e108de6ddbb2fddf3e807a12d0ac58f
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-26 20:05:46 +0530

    UotostringMethod

--- week 7/class 40/ALI SIR/40.js
+++ week 7/class 40/ALI SIR/40.js
@@ -1,5 +1,5 @@
-// Q55. flip word 
-// str = "AnIsH"
+// Q55. flip word  Toggle upcase to low case and cise versesa
+// str = "AnIsH" 
 // ans=""
 // for (let i=0 ; i<str.length ; i++){
 //     if(str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90 ){
@@ -13,4 +13,30 @@
 // }
 // console.log(ans);
 
-//Q.56 
+
+
+//StringMethods
+// let s1 ="AnIsh"
+// console.log("s1 : ",s1)
+// console.log(s1.toUpperCase());
+// console.log(s1.toLowerCase());
+// console.log(s1.concat(" bhattacharya"));
+// console.log(s1.startsWith("Abi"))
+// console.log(s1.endsWith("nIsh"))
+// console.log(s1.includes("a"))
+// console.log(s1.includes("A"))
+// console.log(s1.includes("An"))
+// console.log(s1.includes("nI"))
+// console.log(s1.includes("Ish"))
+// console.log(s1.includes("An"))
+
+// let s2 = " anish%is%in% bhopal% now "
+// console.log(s2.split(""))
+// console.log(s2.split(" "))
+// console.log(s2.split("%"))  /// important check it just breask from % 
+
+ 
+// console.log(s2.trim()) // removes preceeding and extra space form end
+// console.log(s2.trimStart()) // removes preceeding  end
+// console.log(s2.trimEnd())  // removes extra space form end
+
