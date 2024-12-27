85a3fc04dad07345294ac35776db4aca6d24689b
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-27 19:51:30 +0530

    done

--- week 7/class 40/ALI SIR/40.js
+++ week 7/class 40/ALI SIR/40.js
@@ -4,7 +4,7 @@
 // for (let i=0 ; i<str.length ; i++){
 //     if(str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90 ){
 //      ans = ans + String.fromCharCode(str.charCodeAt(i)+32)   
-        
+
 //     }
 //     else {
 //      ans = ans + String.fromCharCode(str.charCodeAt(i)-32)   
@@ -35,7 +35,7 @@
 // console.log(s2.split(" "))
 // console.log(s2.split("%"))  /// important check it just breask from % 
 
- 
+
 // console.log(s2.trim()) // removes preceeding and extra space form end
 // console.log(s2.trimStart()) // removes preceeding  end
 // console.log(s2.trimEnd())  // removes extra space form end
@@ -43,7 +43,35 @@
 
 
 //Q56. count the valid words (sol in copy)
+// let words = ["pay","attire", 'attend',"practise"]
+// let pref = "at"
+// let count = 0 
+// for (let i = 0 ; i<words.length ; i++){
+//     if(words[i].startsWith(pref)) count++
+// }
+// console.log(count);
+
+
+
+
+
+
+
+
+
 //Q.57. split and change (sol in copy)
+let sentence = "hello Bhai Kase h0 App log"
+let words = sentence.split(" ")
+// console.log(words);
+for (let i = 0 ; i<words.length ; i++){
+    console.log(words[i].charAt(0).toUpperCase()+words[i].substring(1));
+    
+}
+        
+
+
+
+
 
 
 // Q58.Count the Frequency of alphabets in the word 
