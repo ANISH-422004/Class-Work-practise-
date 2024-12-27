9a3e747792c04d4caae5766a2fd60602584c4de1
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-27 15:25:33 +0530

    AlisirClassDone

---
+++ week 7/class 41/AliBhaiya/41.js
@@ -0,0 +1,62 @@
+// Q.58 count frequency
+
+// let s = "sdasdafdffddfdfasdfadawqwaaaaaaaaaaaaaAAdawd".toLowerCase()
+
+// let arr = new Array(26).fill(0)
+// for(let i = 0 ; i< s.length ; i++){
+//     let ascii = s.charCodeAt(i)
+//     arr[ascii-97]+=1
+// }
+// for (let i = 0 ; i<arr.length ; i++){
+//     if(arr[i]>0){
+//         console.log(`${String.fromCharCode(i+97)}:${arr[i]}`);
+        
+//     }
+// }
+
+
+//Q.59 Check Anagram
+
+// let s1 = "anish".toLowerCase()
+// let arr1 = new Array(26).fill(0)
+
+// for(let i = 0 ; i< s1.length ; i++){
+//     let ascii = s1.charCodeAt(i)
+//     arr1[ascii-97]+=1
+// }
+// for (let i = 0 ; i<arr1.length ; i++){
+//     if(arr1[i]>0){
+//         console.log(`${String.fromCharCode(i+97)}:${arr1[i]}`);
+        
+//     }
+// }
+
+// let s2 = "anish".toLowerCase()
+// let arr2 = new Array(26).fill(0)
+// for(let i = 0 ; i< s2.length ; i++){
+//     let ascii = s2.charCodeAt(i)
+//     arr2[ascii-97]+=1
+// }
+// for (let i = 0 ; i<arr2.length ; i++){
+//     if(arr2[i]>0){
+//         console.log(`${String.fromCharCode(i+97)}:${arr2[i]}`);
+        
+//     }
+// }
+
+
+// if(s1.length === s2.length){
+//     for(let i= 0 ; i< arr1.length ; i++){
+//         if(arr1[i]!=arr2[i]){
+//             console.log("not anagram");
+//             break        
+//         }
+//     }
+//     console.log("anagram");
+    
+
+// }
+// else{
+//     console.log("not anagram");
+
+// }
