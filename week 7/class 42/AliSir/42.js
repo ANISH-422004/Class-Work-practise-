b765835d1f16a61fdc210c162d70fe4d1d0e8295
ANISH-422004 <anishbhattacharya422004@gmail.com>
2024-12-28 13:35:30 +0530

    classDone

---
+++ week 7/class 42/AliSir/42.js
@@ -0,0 +1,57 @@
+//Revision Class
+
+//////q1.
+// str = prompt("enter a word :")
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str.charAt(i));
+
+// }
+
+
+/////q2.
+// s = prompt("enter a word :")
+// rev=""
+// for(let i = s.length-1; i>=0;i--  ){
+//     rev+=s.charAt(i)
+// }
+
+// console.log(rev);
+
+
+/////q3. plaindromic string or not 
+// s = prompt("WORD:")
+// t = false
+// for (let i = 0; i < Math.floor(s.length / 2); i++) {
+//     console.log( i , s[i] , s.length - 1 - i ,s[s.length - 1 - i]);
+
+//     if (s[i] != s[s.length - 1 - i]) {
+//         t = true
+//         break
+//     }
+// }
+// if (t) {
+//     console.log("non paliandromic");
+
+// } else {
+
+//     console.log("paliandromic");
+// }
+
+
+///////q4. count vowels and consonants
+// s = prompt("word: ").toLowerCase()
+// let v = 0 , c = 0 , space = 0
+// for(let i = 0 ; i<s.length-1 ; i++){
+//     if(s[i]=="a" || s[i]=="e" || s[i]=="i" || s[i]=="o" || s[i]=="u") v++
+//     else if (s[i]==" ") space ++
+//     else c++
+// }
+
+// console.log(`vowles : ${v}`);
+// console.log(`spaces : ${space}`);
+// console.log(`consonants : ${c}`);
+
+
+
+///////q5. toggle character
+// 8
