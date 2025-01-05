d6a5ac8d6b0450bb8fdf7dc0530f3d07f326b59d
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-05 21:56:17 +0530

    done

--- week 8/class 44/Employee Management System/src/components/Others/Alltasks.jsx
+++ week 8/class 44/Employee Management System/src/components/Others/Alltasks.jsx
@@ -8,7 +8,7 @@ const Alltasks = () => {
   return (
     <div className="bg-[#1c1c1c] px-5 mt-5 rounded shadow-md">
       {/* Header Row */}
-      <div className="bg-emerald-400 py-2 px-4 mt-2 flex justify-between items-center rounded hidden md:flex">
+      <div className="bg-emerald-400 py-2 px-4 mt-2 flex justify-between items-center rounded ">
         <h2 className="w-1/5 text-white">Name</h2>
         <h3 className="w-1/5 text-blue-700">New Task</h3>
         <h5 className="w-1/5 text-yellow-500">Active Task</h5>
