e98ed04b07bc74e5831be446f3a04571fc2a1c1d
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-15 13:52:22 +0530

    ClassDone

---
+++ week 10/class 56/ALi Sir/55.js
@@ -0,0 +1,44 @@
+//Q.count frequenct of eements of an araay use Map
+
+// let arr =  [2,3,2,3,2,3,2,4,5,6,8,6,5,1,3,9,90,2]
+// let map = new Map()
+// for (let i = 0; i < arr.length; i++) {
+//     if(map.has(arr[i])){
+//         map.set(arr[i],map.get(arr[i])+1)  
+//     } else{
+//         map.set(arr[i],1)
+//     }
+    
+// }
+
+// console.log(map);
+
+// let arr = [2, 3, 2, 3, 2, 3, 2, 4, 5, 6, 8, 6, 5, 1, 3, 9, 90, 2];
+// let map = new Map();
+
+// for (let i = 0; i < arr.length; i++) {
+//     map.set(arr[i], (map.get(arr[i]) || 0) + 1);       // map.getorDefault 
+// }
+
+// console.log(map);
+
+
+
+// Q.  print the keys whose freque will be 1  in arr
+// let arr = [1,2,3,4,5,5,34,2,2,1,7]
+
+// let map = new Map()
+// for (let i = 0; i < arr.length; i++) {
+//     if(map.has(arr[i])){
+//         map.set(arr[i],map.get(arr[i])+1)  
+//     } else{
+//         map.set(arr[i],1)
+//     }
+    
+// }
+
+
+// for (let key of map.keys()){
+//     if(map.get(key)===1) console.log(key)
+// }
+
