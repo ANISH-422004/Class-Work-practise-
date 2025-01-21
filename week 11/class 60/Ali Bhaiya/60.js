7436f2fa7b44b0e8f77414e8526e53eba2e5c352
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-21 12:42:28 +0530

    ClassDone

---
+++ week 11/class 60/Ali Bhaiya/60.js
@@ -0,0 +1,13 @@
+//Q. rverse a number
+// function reverse(n, rev = 0) {
+//     if (n === 0) return rev;  // Base case: return accumulated reverse
+//     return reverse(Math.floor(n / 10), rev * 10 + (n % 10));
+// }
+
+//Q.fibo sum tll nth term
+// function fibosum(n){
+//     if(n==0 || n==1 ) return n
+//     return fibosum(n-1)+fibosum(n-2)
+// }
+
+// console.log(fibosum(5));
