48dc6061389239dc36c1c315032613acf02f7b58
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-24 15:01:09 +0530

    classDone

---
+++ week 11/class 63/AliBhiaya/63.js
@@ -0,0 +1,18 @@
+//Q. prime or not
+
+// let n = 1
+// let count = 0
+// // if(n<=1) console.log('not prime');
+
+
+// for (let i = 0; i <= n/2; i++) {
+//     if (n % i == 0) {
+//         count++
+//         break;
+//     }
+// }
+// if (count == 1) {
+//     console.log('prime')
+// } else {    
+//     console.log('not prime')
+// }
