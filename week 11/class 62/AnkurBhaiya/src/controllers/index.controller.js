3f1ccc739a2ef990236a2ba789909106e5d08323
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-25 18:02:01 +0530

    formValidation On mongooselevel then learnt about joi package helps to make ModelValidator funtion

--- week 11/class 62/AnkurBhaiya/src/controllers/index.controller.js
+++ week 11/class 62/AnkurBhaiya/src/controllers/index.controller.js
@@ -1,34 +1,46 @@
 const { post } = require("../app")
-const userModel = require("../models/UserModel")
+const { userModel, validateUser } = require("../models/UserModel")
 const postsModel = require("../models/postModel")
 
-module.exports.indexController = (req, res) => {
+module.exports.indexController = async(req, res) => {
+   
     res.render('index')
 }
 
-module.exports.CreateUserController = async(req, res) => {
+module.exports.CreateUserController = async (req, res) => {
     try {
-        const newuser = await userModel.create({
-            name: req.body.name,
-            email: req.body.email,
-            bio: req.body.bio,
-            image: req.body.image
-        })
-        console.log("created user", newuser)
-        res.redirect("/home")
+        let { name, email, bio, image } = req.body
+        let error = validateUser({ name, email, bio, image })
+        if (error) {
+
+            console.log(error.details);
+            
+            console.log(error.details.length > 1 ? error.details[1].message : error.details[0].context.message)
+            res.status(400).send(error.details[0].message)
+
+        } else {
+            console.log("Data is valid")
+            const newuser = await userModel.create({
+                name: req.body.name,
+                email: req.body.email,
+                bio: req.body.bio,
+                image: req.body.image
+            })
+            console.log("created user", newuser)
+            res.redirect("/home")
+        }
     } catch (error) {
         console.log(error)
     }
-    
-    
+
+
 }
 
-module.exports.showPostsController = async(req, res) => {
+module.exports.showPostsController = async (req, res) => {
     try {
         const posts = await postsModel.find()
-       
-        
-        res.render('posts', {posts:posts})
+        const lastUserCreated = await userModel.find()
+        res.render('posts', { posts: posts , lastUserCreated: lastUserCreated[lastUserCreated.length - 1] })
     } catch (error) {
         console.log(error)
     }
@@ -39,8 +51,8 @@ module.exports.showCreatePostController = (req, res) => {
     res.render('createpost')
 }
 
-module.exports.CreatePostcontroller = async(req, res) => {
-    
+module.exports.CreatePostcontroller = async (req, res) => {
+
     try {
         const newpost = await postsModel.create({
             image: req.body.image,
@@ -54,10 +66,10 @@ module.exports.CreatePostcontroller = async(req, res) => {
 }
 
 
-module.exports.likeController = async(req,res)=>{
+module.exports.likeController = async (req, res) => {
     try {
         const dbID = req.params.dbID
-        const postToupdte = await postsModel.findOne({_id:dbID})
+        const postToupdte = await postsModel.findOne({ _id: dbID })
         const updatedPost = await postsModel.findOneAndUpdate(
             postToupdte,
             {
@@ -73,15 +85,15 @@ module.exports.likeController = async(req,res)=>{
 
 }
 
-module.exports.deleteController = async(req,res)=>{
+module.exports.deleteController = async (req, res) => {
     try {
         const dbID = req.params.dbID
-        const deletedPost = await postsModel.findOneAndDelete({_id:dbID})
+        const deletedPost = await postsModel.findOneAndDelete({ _id: dbID })
         console.log("deleted post", deletedPost);
         res.redirect("/home")
-        
+
     } catch (error) {
         console.log(error)
-        
+
     }
 }
--- week 11/class 62/AnkurBhaiya/src/models/UserModel.js
+++ week 11/class 62/AnkurBhaiya/src/models/UserModel.js
@@ -1,25 +1,99 @@
-const mongoose =  require("mongoose")
-
+const mongoose = require("mongoose")
+const joi = require("joi"); //helps in validation of data before saving it to the database the data should be in the form of an object
+const Joi = require("joi");
+//cliet  --> server --> joi --> mongoosevalidaionlayer --> database
 const UserSchema = new mongoose.Schema({
+
     name: {
         type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
+        required: [true, "Name is required"],
+        trim: true,
+        minlength: [3, "Name must be at least 3 characters long"],
+        maxlength: [50, "Name cannot exceed 50 characters"],
     },
     email: {
         type: String,
-        required: true
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [
+            /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            "Please enter a valid email address",
+        ],
     },
-    bio:{
+    image: {
         type: String,
-        required: true
-    }
+        required: [true, "Profile image is required"],
+        default:
+            "https://www.gravatar.com/avatar/?d=identicon", // Default profile image
+    },
+    bio: {
+        type: String,
+        required: [true, "Bio is required"],
+        trim: true,
+        maxlength: [200, "Bio cannot exceed 200 characters"],
+    },
+
+
 
 })
 
-const userModel =  mongoose.model("User", UserSchema)
 
-module.exports = userModel
+
+function validateUser(data) {
+    const schema = Joi.object({
+        name: Joi.string()
+            .trim()
+            .min(3)
+            .max(50)
+            .required()
+            .messages({
+                "string.empty": "Name is required",
+                "string.min": "Name must be at least 3 characters long",
+                "string.max": "Name cannot exceed 50 characters",
+            }),
+
+        email: Joi.string()
+            .trim()
+            .lowercase()
+            .email() // Uses Joi's built-in email validation
+            .custom((value, helpers) => {
+                if (!value.endsWith(".com") && !value.endsWith(".edu")) {
+                    return helpers.error("any.invalid", { message: "Email should end with .com or .edu" });
+                }
+                return value;
+            })
+            .required()
+            .messages({
+                "string.empty": "Email is required",
+            }),
+
+        image: Joi.string()
+            .uri()
+            .required()
+            .messages({
+                "string.empty": "Profile image URL is required",
+                "string.uri": "Please provide a valid URL for the profile image",
+            }),
+
+        bio: Joi.string()
+            .trim()
+            .max(200)
+            .required()
+            .messages({
+                "string.empty": "Bio is required",
+                "string.max": "Bio cannot exceed 200 characters",
+            }),
+    });
+
+    let { error } = schema.validate(data, { abortEarly: false }); // Prevents stopping at the first error
+    return error;
+}
+
+
+
+
+const userModel = mongoose.model("User", UserSchema)
+
+module.exports = { userModel, validateUser }
