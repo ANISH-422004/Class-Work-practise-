c344fba5897867ae0cff684d9f32232ed241b3ad
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-27 11:13:51 +0530

    sunday

--- week 11/SundayPractise/Back-End-Practise/src/controllers/index.controller.js
+++ week 11/SundayPractise/Back-End-Practise/src/controllers/index.controller.js
@@ -1,41 +1,64 @@
 const userModel = require('../models/user.model.js');
 const postModel = require('../models/post.model.js');
+const bcrypt = require('bcrypt');
 
-
-module.exports.indexcontroller  =(req, res) => { 
+module.exports.indexcontroller = (req, res) => {
     res.send("hello")
 }
 
-module.exports.createUserController = async(req, res) => {
-    const {name , email, password} = req.body;
+module.exports.createUserController = async (req, res) => {
+    const { name, email, password } = req.body;
     try {
-        const newUser = new userModel({name, email, password});
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new userModel({ name, email, password: hashedPassword });
         await newUser.save();
         res.status(201).send(newUser);
-    } catch (error) {   
+    } catch (error) {
         console.log(error);
     }
-    
+
 
 }
-
-module.exports.createpostController = async(req, res) => {
+module.exports.loginUserController = async (req, res) => {
     try {
-        const user = await userModel.findOne({name: req.params.username}); // this obj returend by mongoose is a promise which have some methods like save, find, findOne, findOneAndUpdate, findOneAndDelete, update, delete, etc
-        const {content} = req.body;
-        const createdPost = new postModel({user: user._id, content});
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            res.send("User not found"); // instade of  sayng user not found we can say invalid email or password to prevenrt bruteforce attack or data leakeage
+            throw new Error("User not found");
+        } else {
+            const isPassWordValid = await bcrypt.compare(password, user.password);
+            if (isPassWordValid) {
+                res.send(user);
+            } else {
+                res.send("Invalid Password");
+            }
+        }
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+module.exports.createpostController = async (req, res) => {
+    try {
+        const user = await userModel.findOne({ name: req.params.username }); // this obj returend by mongoose is a promise which have some methods like save, find, findOne, findOneAndUpdate, findOneAndDelete, update, delete, etc
+        const { content } = req.body;
+        const createdPost = new postModel({ user: user._id, content });
         await createdPost.save();
-        
+
         user.posts.push(createdPost._id);
         await user.save();
 
 
 
-        res.send(createdPost); 
-        
+        res.send(createdPost);
+
     } catch (error) {
         console.log(error);
-        
+
     }
 
 
--- week 11/SundayPractise/Back-End-Practise/src/routes/index.routes.js
+++ week 11/SundayPractise/Back-End-Practise/src/routes/index.routes.js
@@ -5,6 +5,7 @@ const indexcontroller = require("../controllers/index.controller")
 
 router.get("/", indexcontroller.indexcontroller)
 router.post("/create", indexcontroller.createUserController)
+router.post("/login", indexcontroller.loginUserController)
 router.post("/:username/create/post", indexcontroller.createpostController)
 
 
