a668933c4ab13e7a14917bd4f8cba81cd9fbfd9b
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-28 11:24:34 +0530

    rev

--- week 12/class 64/AliBhaiyaClass/64.js
+++ week 12/class 64/AliBhaiyaClass/64.js
@@ -6,23 +6,23 @@
 // let n = Number(prompt("Enter a number: "));
 
 
-function checkPrime(n) {
-    let arr = new Array(n + 1).fill(true);
-    for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
-        if (arr[i]==true) {
-            for (let j = i * i; j <= n; j += i) { // start j form i*i because all the numbers before i*i are already marked as not prime by the previous numbers. example: 2*3 = 6, 3*2 = 6, so we only need to mark 6 once. 
+// function checkPrime(n) {
+//     let arr = new Array(n + 1).fill(true);
+//     for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+//         if (arr[i]==true) {
+//             for (let j = i * i; j <= n; j += i) { // start j form i*i because all the numbers before i*i are already marked as not prime by the previous numbers. example: 2*3 = 6, 3*2 = 6, so we only need to mark 6 once. 
                 
-                arr[j] = false;
-            }
-        }
-    }
-    let primes = [];
-    for (let i = 2; i <= n; i++) {
-        if (arr[i]) {
-            primes.push(i);
-        }
-    }
-    return primes;
-}
+//                 arr[j] = false;
+//             }
+//         }
+//     }
+//     let primes = [];
+//     for (let i = 2; i <= n; i++) {
+//         if (arr[i]) {
+//             primes.push(i);
+//         }
+//     }
+//     return primes;
+// }
 
-console.log(checkPrime(30));
+// console.log(checkPrime(30));
