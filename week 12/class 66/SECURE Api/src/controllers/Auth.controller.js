7c36a04716e288ced47d4e5d3a0c05f94784dbd3
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-31 02:17:13 +0530

    SecureAPIDone

--- week 12/class 66/SECURE Api/src/controllers/Auth.controller.js
+++ week 12/class 66/SECURE Api/src/controllers/Auth.controller.js
@@ -1,7 +1,7 @@
 const UserModel = require("../models/userModel")
 const bcrypt = require("bcrypt")
 const generateToken = require("../utils/generateToken")
-
+const jwt = require("jsonwebtoken")
 
 module.exports.AuthController = (req, res) => {
     res.send("Auth / route")
@@ -42,49 +42,72 @@ module.exports.registerCoontroller = async (req, res) => {
 module.exports.loginCoontroller = async (req, res) => {
 
     const { email, password } = req.body
-    console.log(email,password)
+    console.log(email, password)
     try {
         let user = await UserModel.findOne({ email })
-        
-    
+
+
         if (!user) return res.status(400).send("Invalid Password or Email")
-    
+
         let result = await bcrypt.compare(password, user.password)
-    
+
         if (!result) {
             return res.send("Invalid Email or Password")
         }
-    
+
         let token = generateToken({ email })
-    
+
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
             maxAge: 30 * 24 * 60 * 60 * 1000,
         })
-    
-    
+
+
         res.send("logged in")
-    
+
     } catch (error) {
         res.send(error.message)
     }
-    
+
 
 }
 
 module.exports.logoutCoontroller = async (req, res) => {
-    
-    
-    res.cookie("token", "" , {
+
+
+    res.cookie("token", "", {
         httpOnly: true,
         secure: true,
-     })
-     res.status(201).send("LoggedOutSuccessfully")
+    })
+    res.status(201).send("LoggedOutSuccessfully")
 }
 
+
+
 module.exports.getUserCoontroller = async (req, res) => {
-   
-    res.send(req.cookies)
-    
-}
+    try {
+
+
+        const decodedToken = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+
+        // Find the user associated with the token's email
+        let user = await UserModel.findOne({ email: decodedToken.email }).select("-password");
+
+        // If user not found, return 404 (Not Found)
+        if (!user) {
+            return res.status(404).send({ error: "User not found." });
+        }
+
+        // Send the response with user data and token (for debugging purposes)
+        res.status(200).send({
+            tokenSentByUser: req.cookies.token, // Note: Make sure you use req.cookies.token
+            user
+        });
+
+    } catch (error) {
+        // Catch JWT or other errors and return a 500 (Internal Server Error)
+        console.error(error); // Logging error for debugging purposes
+        res.status(500).send({ error: "An error occurred while retrieving the user." });
+    }
+};
--- week 12/class 66/SECURE Api/src/middleware/protect.js
+++ week 12/class 66/SECURE Api/src/middleware/protect.js
@@ -3,14 +3,15 @@ const UserModel = require("../models/userModel")
 
 module.exports.protect = async (req, res, next) => {
     if (req.cookies.token) {
+        
         try {
-            const data = jwt.verify(req.cookie.token, process.env.JWT_SECRET)
-            console.log(data);
-            
+
+            const data = jwt.verify(req.cookies.token, process.env.JWT_SECRET)
+
             let user = await UserModel.findOne({ email: data.email }).select("-password")
-            console.log(user);
             
             next()
+
         } catch (error) {
             res.status(401).send("Not Authorized")
         }
