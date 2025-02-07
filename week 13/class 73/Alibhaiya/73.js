e76b1bf32d274f4fbf0fc1f589873f494039ff86
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-02-08 00:41:51 +0530

    insertion sort

--- week 13/class 73/Alibhaiya/73.js
+++ week 13/class 73/Alibhaiya/73.js
@@ -1,18 +1,21 @@
+
+
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        let key = arr[i];
-        let j = i - 1;
-
-        // Move elements of arr[0..i-1], that are greater than key,
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        let key = arr [i]
+        let j = i-1
+        
+        while( j>=0 && arr[j]>key ){
+            arr[j+1]= arr [j]
+            j--
         }
-        arr[j + 1] = key;
+        
+        arr[j+1]=key
+        
     }
-    return arr;
+    return arr
 }
 
-let arr = [12, 11, 13, 5, 6];
+
+let arr = [12, 11, 13, 5, 6,3,10,15,12];
 console.log(insertionSort(arr));
