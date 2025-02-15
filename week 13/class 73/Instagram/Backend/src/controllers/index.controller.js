772a2ad446940a4b9f286e32efdb5090ecef7b39
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-02-15 18:25:29 +0530

    Afdded Aggrigation pipeLine for finding all Post with its Author

--- week 13/class 73/Instagram/Backend/src/controllers/index.controller.js
+++ week 13/class 73/Instagram/Backend/src/controllers/index.controller.js
@@ -1,13 +1,27 @@
 const postModel = require("../models/posts.model")
 const UserModel = require("../models/user.model")
 
-module.exports.feedController = async(req, res) => {
+module.exports.feedController = async (req, res) => {
    try {
-        const posts = await postModel.find()
-        res.status(200).json({posts})
-    
+      const posts = await postModel.aggregate([
+         {
+            '$lookup': {
+               'from': 'users',
+               'localField': 'userId',
+               'foreignField': '_id',
+               'as': 'author'
+            }
+         }, {
+            '$unwind': {
+               'path': '$author'
+            }
+         }
+      ])
+
+      res.status(200).json({ posts })
+
    } catch (error) {
-    console.log(error)
-    res.status(500).json({message:"Internal Server Error"})    
+      console.log(error)
+      res.status(500).json({ message: "Internal Server Error" })
    }
 }
