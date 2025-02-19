2d7e760077782b2ec602cf8f89dc29507714f000
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-02-19 20:59:47 +0530

    HashingDone

---
+++ week 15/class 80/AliBhaiya/80.js
@@ -0,0 +1,19 @@
+//Hassing
+
+//frequrency of elements in array
+
+// let arr = [1,2,3,4,3,132,2,4,2,]
+// let map = new Map()
+// for (let i = 0; i < arr.length; i++) {
+//     const n = arr[i]
+//     if(map.has(n)){
+//         map.set(n , map.get(n)+1)
+//     }else{
+//         map.set(n,1)
+//     }
+    
+// }
+// console.log(map)
+
+
+
