3c5f0bc6f0201cdf42ab79eb9f1fe0f90dfa6414
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-03-31 21:59:55 +0530

    Tower of hanoi Revised

--- week 18/class 91/Ali bhaiya/91.js
+++ week 18/class 91/Ali bhaiya/91.js
@@ -8,14 +8,17 @@
 // The function is expected to print the steps to move n disks from t1 to t2 using t3
 
 
-function towerOfHanoi(n, s, d, h){
-    if(n==0){
-        console.log("move 1 palte form " + s + " to " + d);
+function towerOfHanoi(n, s, h , d) {
+
+    // base case
+    if (n == 0) {
         return;
     }
-    towerOfHanoi(n-1, s, h, d);
-    console.log(n + "[" + s + " -> " + d + "]");
-    towerOfHanoi(n-1, h, d, s);
+
+    towerOfHanoi(n-1 , s, d , h) // move n-1 disks from source to helper using destination as helper
+    console.log(`Move one disk from ${s} to ${d}`) // move nth disk from source to destination
+    towerOfHanoi(n-1 , h, s , d) // move n-1 disks from helper to destination using source as helper
+    // move n-1 disks from helper to destination using source as helper
 
 
 }
