3f6be7f447635f18670720446dd1fa19b4aea97c
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-05-08 01:17:16 +0530

    Access Modeifiers public(default),Private ,protected

--- week 20/TypeScript/app.js
+++ week 20/TypeScript/app.js
@@ -1,19 +1,100 @@
-var a = function () {
-    console.log("Hello");
-};
-a();
-var userRole;
-(function (userRole) {
-    userRole["ADMIN"] = "admin";
-    userRole["USER"] = "user";
-})(userRole || (userRole = {}));
-var obj = {
-    name: "John",
-    age: 20
-};
-obj.name = "Doe";
-console.log(obj.name);
-function getUser(user) {
-    return "Name : ".concat(user.name, " Age : ").concat(user.age, " gender : ").concat(user.gender ? user.gender : "Not specified");
-}
-console.log(getUser({ name: "John", age: 20 }));
+// let a = () => {
+//     console.log("Hello")
+// }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// a()
+// enum userRole {
+//     ADMIN = "admin",
+//     USER = "user",
+// }
+// const obj: { name: string, age: number } = {
+//     name: "John",
+//     age: 20
+// }
+// obj.name = "Doe"
+// console.log(obj.name)
+// interface User {
+//     name: string,
+//     age: number,
+//     gender?: string, // required property
+// }
+// interface User2 {
+//     name: string,
+//     age: number,
+//     gender?: string, // optional property
+//     [key: string]: any // index signature : allows any additional properties    
+// }
+// function getUser(user: User): string {
+//     return `Name : ${user.name} Age : ${user.age} gender : ${user.gender ? user.gender : "Not specified"}`
+// }
+// // console.log(getUser({name: "John" , age: 20})) 
+// // 2 interface with same name
+// // 1. interface with same name will be merged
+// interface abcd {
+//     name: string,
+//     age: number,
+// }
+// interface abcd {
+//      gender?: string,
+// }
+// let obj2: abcd = { 
+//     name: "John",
+//     age: 20,
+// }
+var User = /** @class */ (function () {
+    function User(name, password, role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+    User.prototype.greet = function () {
+        console.log("Hi, I\u2019m ".concat(this.name));
+    };
+    User.prototype.showPassword = function () {
+        console.log("Password is ".concat(this.password));
+    };
+    User.prototype.showRole = function () {
+        console.log("Role is ".concat(this.role));
+    };
+    return User;
+}());
+var subUser = /** @class */ (function (_super) {
+    __extends(subUser, _super);
+    function subUser(name, password, role) {
+        return _super.call(this, name, password, role) || this;
+    }
+    subUser.prototype.showRole = function () {
+        console.log("Role is ".concat(this.role)); // ✅ OK (protected)
+    };
+    // public showPassword() {
+    //     console.log(`Password is ${this.password}`); // ❌ Error (private)
+    // }
+    subUser.prototype.showAdmin = function () {
+        console.log("Role is ".concat(this.role)); // ✅ OK (protected)
+    };
+    return subUser;
+}(User));
+var subUser1 = new subUser("anish", "1234", "user");
+subUser1.showRole();
+// ✅ OK (protected)
+// subUser1.showPassword() // shows error while compilation 
+subUser1.showAdmin();
+var user = new User("Anish", "123456", "user");
+user.greet(); // ✅ OK
+console.log(user.name); // ✅ OK (public)
+// console.log(user.password); ❌ Error (private)
+// console.log(user.role);     ❌ Error (protected)
--- week 20/TypeScript/app.ts
+++ week 20/TypeScript/app.ts
@@ -1,99 +1,126 @@
 
-let a = () => {
-    console.log("Hello")
-}
+// let a = () => {
+//     console.log("Hello")
+// }
 
-a()
+// a()
 
-enum userRole {
-    ADMIN = "admin",
-    USER = "user",
-}
-
-
-const obj: { name: string, age: number } = {
-    name: "John",
-    age: 20
-}
-
-obj.name = "Doe"
-
-console.log(obj.name)
+// enum userRole {
+//     ADMIN = "admin",
+//     USER = "user",
+// }
 
 
-interface User {
-    name: string,
-    age: number,
-    gender?: string, // required property
-}
-interface User2 {
-    name: string,
-    age: number,
-    gender?: string, // optional property
-    [key: string]: any // index signature : allows any additional properties    
-}
+// const obj: { name: string, age: number } = {
+//     name: "John",
+//     age: 20
+// }
+
+// obj.name = "Doe"
+
+// console.log(obj.name)
 
 
-function getUser(user: User): string {
-    return `Name : ${user.name} Age : ${user.age} gender : ${user.gender ? user.gender : "Not specified"}`
-}
+// interface User {
+//     name: string,
+//     age: number,
+//     gender?: string, // required property
+// }
+// interface User2 {
+//     name: string,
+//     age: number,
+//     gender?: string, // optional property
+//     [key: string]: any // index signature : allows any additional properties    
+// }
 
 
-// console.log(getUser({name: "John" , age: 20})) 
+// function getUser(user: User): string {
+//     return `Name : ${user.name} Age : ${user.age} gender : ${user.gender ? user.gender : "Not specified"}`
+// }
 
 
-// 2 interface with same name
-// 1. interface with same name will be merged
+// // console.log(getUser({name: "John" , age: 20})) 
 
 
-interface abcd {
-    name: string,
-    age: number,
-}
+// // 2 interface with same name
+// // 1. interface with same name will be merged
 
 
-interface abcd {
-     gender?: string,
-}
+// interface abcd {
+//     name: string,
+//     age: number,
+// }
 
 
-let obj2: abcd = { 
-    name: "John",
-    age: 20,
+// interface abcd {
+//      gender?: string,
+// }
 
-}
+
+// let obj2: abcd = { 
+//     name: "John",
+//     age: 20,
+
+// }
 
 
 
 class User {
-    public name: string;        // Accessible everywhere
-    private password: string;   // Only inside this class
-    protected role: string;     // Inside this class + subclasses
-  
+    public name: string;
+    private password: string;
+    protected role: string;
+
+
     constructor(name: string, password: string, role: string) {
-      this.name = name;
-      this.password = password;
-      this.role = role;
+        this.name = name;
+        this.password = password;
+        this.role = role;
     }
-  
+
     public greet() {
-      console.log(`Hi, I’m ${this.name}`);
+        console.log(`Hi, I’m ${this.name}`);
     }
-  
+
     private showPassword() {
-      console.log(`Password is ${this.password}`);
+        console.log(`Password is ${this.password}`);
     }
-  
+
     protected showRole() {
-      console.log(`Role is ${this.role}`);
+        console.log(`Role is ${this.role}`);
     }
-  }
+}
+
+class subUser extends User {
+    constructor(name: string, password: string, role: string) {
+        super(name, password, role);
+    }
+
+    public showRole() {
+        console.log(`Role is ${this.role}`); // ✅ OK (protected)
+    }
+
+    // public showPassword() {
+    //     console.log(`Password is ${this.password}`); // ❌ Error (private)
+    // }
+
+    public showAdmin(){
+        console.log(`Role is ${this.role}`); // ✅ OK (protected)
+    }
 
 
-  const user = new User("Anish", "123456", "user");
-user.greet();                // ✅ OK
-console.log(user.name);      // ✅ OK (public)
+}
+
+
+const subUser1 = new subUser("anish" , "1234" , "user")
+subUser1.showRole(); 
+// ✅ OK (protected)
+// subUser1.showPassword() // shows error while compilation 
+subUser1.showAdmin()
+
+
+const user = new User("Anish", "123456", "user");
+// user.greet();                // ✅ OK
+// console.log(user.name);      // ✅ OK (public)
 // console.log(user.password); ❌ Error (private)
 // console.log(user.role);     ❌ Error (protected)
 
-  
